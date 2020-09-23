a0925b04c8e2faeaaa5a88e086f885962cbdfa72
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-23 20:00:14 +0530

    route is used

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import {Error} from './Error';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    return (
+      <>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route component={Error}></Route>
+      </Switch>
+      </>
+    );
+
 }
 
 export default App;
---
+++ src/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error=()=>{
+    return(<>
+        <h2>404 Not Found!</h2>
+    </>);
+}
+
+export {Error};
